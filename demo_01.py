bb437d8870504a35254d97998ebbacdbae98248e
MizunagiKB <mizukb@LIVE.JP>
2020-09-05 12:10:33 +0900

    ベクトルの計算部分の説明を追加しました。

--- demo_01.py
+++ demo_01.py
@@ -1,188 +1,122 @@
-import pyxel
-import random
 import math
+import random
+import pyxel
 from lib import *
 
 SCREEN_W = 256
 SCREEN_H = 240
-
-list_render = []
-list_laser = []
-list_enemy = []
-list_crash = []
-
-
-class CEnemy:
-    SIZE = 16
-
-    def __init__(self, vct_position):
-        self.vct_position = vct_position
-        self.speed = random.randint(1, 6)
-        self.enable = True
-
-    def update(self):
-        if self.vct_position.y < (SCREEN_H + self.SIZE):
-            list_render.append(self)
-        else:
-            self.enable = False
-
-        self.vct_position.y += self.speed
-
-    def render(self):
-        pyxel.rect(
-            self.vct_position.x - 8, self.vct_position.y - 8, self.SIZE, self.SIZE, 13
-        )
-
-
-class CCrash:
-    COLOR = [10, 9, 8, 10]
-    LIMIT = 5
-
-    def __init__(self, vct_position):
-        self.vct_position = vct_position
-        self.counter = 0
-        self.enable = True
-
-    def update(self):
-        if self.counter < self.LIMIT:
-            list_render.append(self)
-        else:
-            self.enable = False
-        self.counter += 1
-
-    def render(self):
-        try:
-            col = self.COLOR[self.counter]
-        except:
-            col = self.COLOR[-1]
-
-        pyxel.circ(
-            self.vct_position.x,
-            self.vct_position.y,
-            math.sin(math.radians(self.counter)) * SCREEN_W,
-            col,
-        )
-
-
-class CLaser:
-    COLOR = [7, 6, 12, 5, 1]
-    LIMIT = 90
-    LINE_SIZE = 8
-
-    def __init__(self, vct_position):
-        self.vct_position = vct_position
-        self.list_position = []
-        self.vct_direction = Vector2(0, -1)
-        self.speed = 4
-        self.deg_limit = 2
-        self.counter = 0
-        self.enable = True
-
-    def set_target(self, vct_target):
-        self.vct_target = vct_target
-
-    def update(self):
-        global list_enemy
-
-        v = self.vct_target - self.vct_position
-
-        for enemy in list_enemy:
-            if enemy.enable is True:
-                if (enemy.vct_position - self.vct_position).length() < CEnemy.SIZE:
-                    list_crash.append(
-                        CCrash(Vector2(self.vct_position.x, self.vct_position.y))
-                    )
-                    enemy.enable = False
-                    self.enable = False
-
-        v.normalize()
-        r = self.vct_direction.angle_to(v)
-
-        if self.counter > 15:
-            self.deg_limit = 30
-            self.speed = 16
-        elif self.counter > (self.LIMIT - 30):
-            self.deg_limit = 90
-
-        minr = math.radians(self.deg_limit)
-        maxr = math.radians(-self.deg_limit)
-        r = min(max(r, maxr), minr)
-
-        self.vct_direction = self.vct_direction.rotated(r)
-        self.vct_direction.normalize()
-
-        self.vct_position += self.vct_direction * self.speed
-
-        self.list_position.insert(0, Vector2(self.vct_position.x, self.vct_position.y))
-        if len(self.list_position) > self.LINE_SIZE:
-            self.list_position = self.list_position[0 : self.LINE_SIZE]
-
-        if self.counter < self.LIMIT:
-            list_render.append(self)
-        else:
-            self.enable = False
-        self.counter += 1
-
-    def render(self):
-
-        x1 = self.list_position[0].x
-        y1 = self.list_position[0].y
-
-        for n, o in enumerate(self.list_position):
-
-            try:
-                col = self.COLOR[n]
-            except:
-                col = self.COLOR[-1]
-
-            pyxel.line(x1, y1, o.x, o.y, col)
-            x1 = o.x
-            y1 = o.y
+CENTER_X = SCREEN_W // 2
+CENTER_Y = SCREEN_H // 2
 
 
 def main():
-    global list_render
-    global list_laser
-    global list_crash
-    global list_enemy
 
     pyxel.init(SCREEN_W, SCREEN_H)
     pyxel.mouse(True)
 
-    while 1:
-        pyxel.cls(0)
+    # 中心座標
+    vct_center = Vector2(CENTER_X, CENTER_Y)
 
-        if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
-            o = CLaser(Vector2(random.randint(0, SCREEN_W), SCREEN_W))
-            list_laser.append(o)
+    # 角度制限の初期値
+    limit = math.radians(45)
+    rotation = 0
 
-        o = CEnemy(Vector2(random.randint(0, SCREEN_W), -CEnemy.SIZE))
-        list_enemy.append(o)
+    back_color = 0
+    text_color = 0
 
-        for o in list_enemy:
-            o.update()
+    while True:
 
-        for o in list_laser:
-            o.set_target(Vector2(pyxel.mouse_x, pyxel.mouse_y))
-            o.update()
+        # 計算基準となるベクトルを作成
+        dir_basic = Vector2(1, 0).rotated(math.radians(rotation))
+        dir_basic.normalize()
 
-        for o in list_crash:
-            o.update()
+        # 中心からみてマウスの方向を向いているベクトルを作成
+        vct_mouse = Vector2(pyxel.mouse_x, pyxel.mouse_y)
+        dir_mouse = vct_mouse - vct_center
+        dir_mouse.normalize()
 
-        [o.render() for o in list_render]
+        # dir_basicとdir_mouseの角度を計算。
+        # angle_toの計算結果はラジアンとして戻ります。
+        raw = dir_basic.angle_to(dir_mouse)
+        fix = min(max(raw, -limit), limit)
 
-        pyxel.text(8, 8, "Render(s) {:4d}".format(len(list_render)), 7)
-        pyxel.text(8, 16, "Enemy(s)  {:4d}".format(len(list_enemy)), 7)
-        pyxel.text(8, 24, "Laser(s)  {:4d}".format(len(list_laser)), 7)
-        pyxel.text(8, 32, "Crash(s)  {:4d}".format(len(list_crash)), 7)
+        # 内積の計算。
+        # 二つのベクトルの内積を求めると計算相手の向き（前後）を調べることが出来ます。
+        dot = dir_basic.dot(dir_mouse)
+        # 外積を計算。
+        # 二つのベクトルの外積を求めると計算相手の向き（左右）を調べることが出来ます。
+        crs = dir_basic.cross(dir_mouse)
 
-        list_enemy = [o for o in list_enemy if o.enable is True]
-        list_laser = [o for o in list_laser if o.enable is True]
-        list_crash = [o for o in list_crash if o.enable is True]
-        list_render = []
+        if pyxel.btnp(pyxel.KEY_SPACE) == 1:
+            rotation = 0
+            limit = math.radians(45)
+
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON) == 1:
+            rotation += 15
+            if rotation == 360:
+                rotation = 0
+
+        if pyxel.btnp(pyxel.MOUSE_RIGHT_BUTTON) == 1:
+            limit = abs(raw)
+
+        back_color = 0
+        if abs(raw) > limit:
+            back_color = 1
+
+        text_color = 7
+        if dot < 0:
+            text_color = 8
+
+        # 描画処理部
+        pyxel.cls(back_color)
+
+        pyxel.line(
+            CENTER_X,
+            CENTER_Y,
+            CENTER_X + dir_basic.x * 64,
+            CENTER_Y + dir_basic.y * 64,
+            5,
+        )
+
+        vct_calc = dir_basic.rotated(fix)
+        pyxel.line(
+            CENTER_X,
+            CENTER_Y,
+            CENTER_X + vct_calc.x * 64,
+            CENTER_Y + vct_calc.y * 64,
+            12,
+        )
+
+        vct_calc = dir_basic.rotated(raw)
+        pyxel.line(
+            CENTER_X,
+            CENTER_Y,
+            CENTER_X + dir_mouse.x * 64,
+            CENTER_Y + dir_mouse.y * 64,
+            7,
+        )
+
+        list_text = [
+            "     mouse: {:7.2f} {:7.2f}".format(pyxel.mouse_x, pyxel.mouse_y),
+            " dir_mouse: {:7.2f} {:7.2f}".format(dir_mouse.x, dir_mouse.y),
+            "angle(rad): {:7.2f}".format(raw),
+            "angle(deg): {:7.2f}".format(math.degrees(raw)),
+            "       dot: {:7.2f} [{:5s}]".format(dot, "front" if dot > 0 else "back"),
+            "     cross: {:7.2f} [{:5s}]".format(crs, "right" if crs > 0 else "left"),
+            "     frame: {:4d}".format(pyxel.frame_count),
+        ]
+        for n, text in enumerate(list_text):
+            pyxel.text(8, 8 * n + 8, text, text_color)
+
+        pyxel.text(8, 208, "  rotation: {:4d}".format(rotation), 5)
+        pyxel.text(8, 216, "limit(rad): {:7.2f}".format(limit), 12)
+        pyxel.text(8, 224, "limit(deg): {:7.2f}".format(math.degrees(limit)), 12)
 
         pyxel.flip()
 
 
 if __name__ == "__main__":
     main()
+
+
+# [EOF]
